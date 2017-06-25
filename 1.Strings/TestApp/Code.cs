e428bf3df2c27b4e1f21c4d140690ec70b2ef8d4
TheProxyRe <theproxyjomgegar@gmail.com>
2017-06-25 13:49:49 +0200

    Strings Tutorial Preparation

---
+++ 1.Strings/TestApp/Code.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace TestAppStrings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var password = Console.ReadLine();
+            if (password == "Tutorial 1")
+                Console.WriteLine("Well Done");
+            else
+                Console.WriteLine("Wrong Password");
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
+        }
+    }
+}
